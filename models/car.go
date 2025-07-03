9bf6a39fdce5f848a7b5a85b190b4eafa60cd82a
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2025-07-03 14:22:35 +0530

    feat: UC10 - implement handicap priority parking strategy

--- models/car.go
+++ models/car.go
@@ -1,6 +1,8 @@
 package models
 
 type Car struct {
+    Size       VehicleSize
+    IsHandicap bool
 	LicensePlate string
 	DriverName   string
 }
@@ -10,4 +12,34 @@ func NewCar(licensePlate, driverName string) *Car {
 		LicensePlate: licensePlate,
 		DriverName:   driverName,
 	}
+}
+
+// Enhanced car properties for advanced parking strategies
+type VehicleSize int
+
+const (
+    SmallVehicle VehicleSize = iota
+    MediumVehicle
+    LargeVehicle
+)
+
+func (c *Car) SetVehicleSize(size VehicleSize) {
+    c.Size = size
+}
+
+func (c *Car) SetHandicapStatus(isHandicap bool) {
+    c.IsHandicap = isHandicap
+}
+
+func (c *Car) GetVehicleSizeString() string {
+    switch c.Size {
+    case SmallVehicle:
+        return "Small"
+    case MediumVehicle:
+        return "Medium"
+    case LargeVehicle:
+        return "Large"
+    default:
+        return "Medium"
+    }
 }
